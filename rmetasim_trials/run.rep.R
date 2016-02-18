a6ebf6f0b87bc2583a66d751758a5c55431ddcea
Allan Strand <stranda@gmail.com>
2016-02-18 17:06:31 -0500

    added segmented regression also fixed some issues with run.rep

--- rmetasim_trials/run.rep.R
+++ rmetasim_trials/run.rep.R
@@ -1,12 +1,12 @@
-#
-#
-# run a single rep of a simulation.
-#  this includes make a landscape, simulate deep history with simcoal and
-#  then simulate 500 gens with rmetasim
-#
-#
+###
+###
+### run a single rep of a simulation.
+###  this includes make a landscape, simulate deep history with simcoal and
+###  then simulate 500 gens with rmetasim
+###
+###
 
-run.rep <- function(refugia=c(1,3),
+run.rep <- function(refugia=c(1,2),
                     refsize=c(1000,1000),
                     dens.scale=0.0005,
                     samp.per.pop=24,
@@ -18,14 +18,46 @@ run.rep <- function(refugia=c(1,3),
                     nloc=10,
                     nmut=10e-4,
                     h=100,
-                    k=rep(100,h),
+                    k=rep(1000,h),
                     e=rep(0,h),
-                    shortshape=1,shortscale=1,longmean=10,mix=0,
-                    executable="fsc251"
+                    shortshape=1,shortscale=1,longmean=3,mix=0,
+                    executable="fsc251",
+                    glac.front = 2,
+                    glac.retreat.per.epoch=1,
+                    decay.dist=3,
+                    marginal.decrease=0.8,
+                    plotland=F
                     )
     {
-
+        if (FALSE)
+            {
+               refugia=c(1,2)
+               refsize=c(1000,1000)
+               dens.scale=0.0005
+               samp.per.pop=24
+               gens.per.epoch=50
+               epochs=10
+               splittime=120000
+               cpmut=10e-08
+               cpseq=500
+               nloc=10
+               nmut=10e-4
+               h=100
+               k=rep(250,h)
+               e=rep(0,h)
+               shortshape=1
+               shortscale=1
+               longmean=10
+               mix=0
+               executable="fsc251"
+               glac.front =15
+               glac.retreat.per.epoch=1
+               decay.dist=3
+               marginal.decrease=0.5
+               plotland=T
+            }
         l <- recolonizeLandscape(refs=refugia,sizeref=refsize,dens.scale=dens.scale,
+                                 k=k,e=e,
                                  cpmut=cpmut,nssr=nloc,ssrmut=nmut,
                                  shortshape=shortshape,
                                  shortscale=shortscale,
@@ -63,13 +95,45 @@ run.rep <- function(refugia=c(1,3),
                                           x
                                       })
 #################
+################# actually do the rmetasim simulations
 #################
 
+if (plotland){require(lattice); plots <- vector("list",epochs)}
+        
         for (g in 1:epochs)
             {
                 print(g)
+                ##set carry to correspond to glaciated front
+                crd <- landscape.popcoord(l)
+                crd$carry.fact <- 1.0
+#                crd$carry.fact[(glac.front-crd$row)>decay.dist] <- 1.0  
+                crd$carry.fact[((glac.front)-crd$row)<=decay.dist] <- (1/decay.dist)*((glac.front)-crd$row[((glac.front)-crd$row)<=decay.dist])
+                crd$carry.fact[crd$row>=(glac.front)] <- 0
+                crd$carry.fact[crd$col%in%c(1,max(crd$col))] <- crd$carry.fact[crd$col%in%c(1,max(crd$col))]*marginal.decrease
+                l$demography$epochs[[1]]$Carry <- k*crd$carry.fact
+                print(unique(crd[,c("row","carry.fact")]))
                 l <- landscape.simulate(l,gens.per.epoch)
+                
+                if (plotland)
+                    {
+                        print(paste("glac front",glac.front))
+                        tbl <- data.frame(table(landscape.populations(l)))
+                        names(tbl)[1] <- "pop"
+                        plotpops <- merge(tbl,crd)
+                        plots[[g]] <- levelplot(Freq~col+row,data=plotpops,xlim=c(0,max(crd$row)+1),
+                                                ylim=c(0,max(crd$col)+1),
+                                                panel=function(...){panel.levelplot(...)
+                                                                    panel.abline(h=glac.front)
+                                                                }
+                                                )
+                    }
+                    
+                glac.front <- glac.front+glac.retreat.per.epoch
             }
+
+#################
+#################
+#################
 
         sampland <- landscape.sample(l,ns=samp.per.pop)
 
@@ -83,5 +147,8 @@ run.rep <- function(refugia=c(1,3),
         aln@ind.info=data.frame(sampland$individuals[,c(1,4,5,6)])
         names(aln@ind.info) <- c("state","ID","MatID","PatID")
         #return gi, gi for cp, alignmnent for cp and the sampled landscape
-        list(nucgi=gi,cpgi=multidna2genind(aln,mlst=T),cpaln=aln,land=sampland)
+        if (plotland)
+            list(nucgi=gi,cpgi=multidna2genind(aln,mlst=T),cpaln=aln,land=sampland,plots=plots)
+        else
+            list(nucgi=gi,cpgi=multidna2genind(aln,mlst=T),cpaln=aln,land=sampland)
     }
